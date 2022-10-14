f19d89de05863722cc5fbb193773379e5caa37ee
Kalyani Modi <kalyanim@webmob.tech>
2022-10-14 11:50:00 +0530

    Remove duplicates

---
+++ Kalyani Modi/RemoveDuplicates.js
@@ -0,0 +1,18 @@
+function getUnique(arr){
+    
+    let uniqueArr = [];
+    
+    // loop through array
+    for(let i of arr) {
+        if(uniqueArr.indexOf(i) === -1) {
+            uniqueArr.push(i);
+        }
+    }
+    console.log(uniqueArr);
+}
+
+const array = [1, 2, 3, 2, 3];
+
+// calling the function
+// passing array argument
+getUnique(array);

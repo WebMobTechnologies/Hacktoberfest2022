fe88b915d92f6e3f0480fe232afc5328fc3bd6cc
Kalyani Modi <kalyanim@webmob.tech>
2022-10-14 12:35:57 +0530

    Done changes like add output

--- Kalyani Modi/RemoveDuplicates.js
+++ Kalyani Modi/RemoveDuplicates.js
@@ -14,3 +14,10 @@ function getUnique(arr) {
 const array = [1, 2, 3, 2, 3, 5, 6, 6];
 getUnique(array);
 
+/* 
+For Example :
+
+Input : array = [1, 2, 3, 2, 3, 5, 6, 6]
+
+Output:[1, 2, 3, 5, 6]
+*/
